b3633d86716b2c424f796e20f2e6ff792a2ed186
Sean Eddy <sean@eddylab.org>
2006-03-03 13:25:27 +0000

    added esl_ssi.c; ssi port from squid still in progress.
    git-svn-id: https://svn.janelia.org/eddylab/eddys/easel/trunk@97 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

---
+++ esl_ssi.c
@@ -0,0 +1,20 @@
+/* esl_ssi.c
+ * simple sequence indices: fast lookup in large sequence files by keyword.
+ * 
+ * SVN $Id$
+ * adapted from squid's ssi.c
+ * SRE, Thu Mar  2 18:46:02 2006 [St. Louis]
+ */
+
+
+
+
+
+
+
+
+
+
+/*****************************************************************
+ * @LICENSE@
+ *****************************************************************/
