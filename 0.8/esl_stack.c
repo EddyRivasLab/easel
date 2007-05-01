fc03b18cbb8694de8955c31d216047ac33b9ea8c
Sean Eddy <sean@eddylab.org>
2007-05-01 13:35:20 +0000

    easel: created Infernal 0.8 branch from tag
    git-svn-id: https://svn.janelia.org/eddylab/eddys/easel/branches/infernal@178 db3e70e4-12e1-0310-b23f-81a6e3d09ba7

---
+++ 0.8/esl_stack.c
@@ -0,0 +1,521 @@
+/* stack.c
+ * SRE 1 March 2000 [Seattle]
+ * Incorp into Easel SRE, Sun Dec 26 07:42:12 2004 [Zaragoza]
+ *
+ * Implementations of pushdown stacks for integers, pointers, and characters.
+ *
+ * Stacks are kept as growable arrays. A stack's memory is
+ * grown when necessary by adding some block size. The 
+ * initial allocation and the block size are set to 100
+ * by default. The block size can be changed by the caller.
+ * 
+ *****************************************************************
+ *
+ * Basic API (using integer stack as an example):
+ * 
+ *   say I want to push the numbers 42, 7, and 3 onto a stack,
+ *   then pop them off and print them: 
+ *   
+ *   #include <easel.h>
+ *   #include <esl_stack.h>
+ *
+ *   ESL_STACK *ns;
+ *   int       x;
+ *
+ *   ns = esl_stack_ICreate();
+ *   esl_stack_IPush(ns, 42);
+ *   esl_stack_IPush(ns, 7);
+ *   esl_stack_IPush(ns, 3);
+ *   while (esl_stack_IPop(ns, &x) != eslEOD) 
+ *      printf("%d\n", x);
+ *   esl_stack_Destroy(ns);   
+ * 
+ * Diagnostics:
+ *   Create() functions return NULL on an allocation failure.
+ *   Push()   functions throw  eslEMEM on an allocation failure.
+ *   Pop()    functions return eslEOD when the stack is empty.
+ *
+ * Other functions:
+ *   esl_stack_ObjectCount(ns)    :  returns # of objects in the stack
+ *   esl_stack_Convert2String(ns) :  converts a char stack to a string, destroying the stack.
+ *
+ * SVN $Id$
+ */ 
+#include <esl_config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <easel.h>
+#include <esl_stack.h>
+
+
+/* Function:  esl_stack_ICreate()
+ * Incept:    SRE, Sun Dec 26 09:11:50 2004 [Zaragoza]
+ *
+ * Purpose:   Creates an integer stack.
+ *
+ * Returns:   a pointer to the new stack.
+ *
+ * Throws:    <NULL> on an allocation failure.
+ */
+ESL_STACK *
+esl_stack_ICreate(void)
+{
+  int status;
+  ESL_STACK *ns = NULL;
+  
+  ESL_ALLOC(ns, sizeof(ESL_STACK));
+  ns->nalloc   = ESL_STACK_INITALLOC;
+  ns->pdata    = NULL;
+  ns->cdata    = NULL;
+  ESL_ALLOC(ns->idata, sizeof(int) * ns->nalloc);
+  ns->n        = 0;
+  return ns;
+
+ ERROR:
+  esl_stack_Destroy(ns);
+  return NULL;
+}
+
+/* Function:  esl_stack_CCreate()
+ * Incept:    SRE, Sun Dec 26 09:15:35 2004 [Zaragoza]
+ *
+ * Purpose:   Creates a character stack.
+ *
+ * Returns:   a pointer to the new stack.
+ *
+ * Throws:    <NULL> on an allocation failure.
+ */
+ESL_STACK *
+esl_stack_CCreate(void)
+{
+  int status;
+  ESL_STACK *cs = NULL;
+  
+  ESL_ALLOC(cs, sizeof(ESL_STACK));
+  cs->nalloc   = ESL_STACK_INITALLOC;
+  cs->idata    = NULL;
+  cs->pdata    = NULL;
+  ESL_ALLOC(cs->cdata, sizeof(char) * cs->nalloc);
+  cs->n        = 0;
+  return cs;
+
+ ERROR:
+  esl_stack_Destroy(cs);
+  return NULL;
+}
+
+/* Function:  esl_stack_PCreate()
+ * Incept:    SRE, Sun Dec 26 09:16:07 2004 [Zaragoza]
+ *
+ * Purpose:   Creates a pointer stack.
+ *
+ * Returns:   a pointer to the new stack.
+ *
+ * Throws:    <NULL> on an allocation failure.
+ */
+ESL_STACK *
+esl_stack_PCreate(void)
+{
+  int status;
+  ESL_STACK *ps = NULL;
+  
+  ESL_ALLOC(ps, sizeof(ESL_STACK));
+  ps->nalloc   = ESL_STACK_INITALLOC;
+  ps->idata    = NULL;
+  ps->cdata    = NULL;
+  ESL_ALLOC(ps->pdata, sizeof(void *) * ps->nalloc);
+  ps->n        = 0;
+  return ps;
+
+ ERROR:
+  esl_stack_Destroy(ps);
+  return NULL;
+}
+
+/* Function:  esl_stack_Reuse()
+ * Incept:    SRE, Tue Dec 28 04:21:36 2004 [Zaragoza]
+ *
+ * Purpose:   Empties stack <s> so it can be reused without
+ *            creating a new one. The stack <s>
+ *            can be of any data type; it retains its original
+ *            type.
+ *
+ * Returns:   <eslOK>
+ */
+int
+esl_stack_Reuse(ESL_STACK *s)
+{
+  s->n = 0;	/* it's that simple in this implementation */
+  return eslOK;
+}
+
+/* Function:  esl_stack_Destroy()
+ * Incept:    SRE, Sun Dec 26 09:16:24 2004 [Zaragoza]
+ *
+ * Purpose:   Destroys a created stack <s>, of any data type.
+ */
+void
+esl_stack_Destroy(ESL_STACK *s)
+{
+  if (s->idata != NULL) free(s->idata);
+  if (s->cdata != NULL) free(s->cdata);
+  if (s->pdata != NULL) free(s->pdata);
+  free(s);
+}
+
+/* Function:  esl_stack_IPush()
+ * Incept:    SRE, Sun Dec 26 09:17:17 2004 [Zaragoza]
+ *
+ * Purpose:   Push an integer <x> onto an integer stack <ns>.
+ *
+ * Returns:   <eslOK> on success.
+ *
+ * Throws:    <eslEMEM> on reallocation failure.
+ */
+int
+esl_stack_IPush(ESL_STACK *ns, int x)
+{
+  int  status;
+  int *ptr;
+
+  if (ns->n == ns->nalloc) {
+    ESL_RALLOC(ns->idata, ptr, sizeof(int) * ns->nalloc * 2);
+    ns->nalloc += ns->nalloc;	/* reallocate by doubling */
+  }
+  ns->idata[ns->n] = x;
+  ns->n++;
+  return eslOK;
+
+ ERROR:
+  return status;
+}
+
+/* Function:  esl_stack_CPush()
+ * Incept:    SRE, Sun Dec 26 09:18:24 2004 [Zaragoza]
+ *
+ * Purpose:   Push a character <c> onto a character stack <cs>.
+ *
+ * Returns:   <eslOK> on success.
+ *
+ * Throws:    <eslEMEM> on reallocation failure.
+ */
+int
+esl_stack_CPush(ESL_STACK *cs, char c)
+{
+  int  status;
+  char *ptr;
+
+  if (cs->n == cs->nalloc) {
+    ESL_RALLOC(cs->cdata, ptr, sizeof(char) * cs->nalloc * 2);
+    cs->nalloc += cs->nalloc;	/* reallocate by doubling */
+  }
+  cs->cdata[cs->n] = c;
+  cs->n++;
+  return eslOK;
+
+ ERROR:
+  return status;
+}
+
+/* Function:  esl_stack_PPush()
+ * Incept:    SRE, Sun Dec 26 09:18:49 2004 [Zaragoza]
+ *
+ * Purpose:   Push a pointer <p> onto a pointer stack <ps>.
+ *
+ * Returns:   <eslOK> on success.
+ *
+ * Throws:    <eslEMEM> on reallocation failure.
+ */
+int
+esl_stack_PPush(ESL_STACK *ps, void *p)
+{
+  int status;
+  void *ptr;
+
+  if (ps->n == ps->nalloc) {
+    ESL_RALLOC(ps->pdata, ptr, sizeof(void *) * ps->nalloc * 2);
+    ps->nalloc += ps->nalloc;	/* reallocate by doubling */
+  }
+  ps->pdata[ps->n] = p;
+  ps->n++;
+  return eslOK;
+
+ ERROR:
+  return status;
+}
+
+/* Function:  esl_stack_IPop()
+ * Incept:    SRE, Sun Dec 26 09:19:12 2004 [Zaragoza]
+ *
+ * Purpose:   Pops an integer off the integer stack <ns>, and returns
+ *            it through <ret_x>.
+ *
+ * Returns:   <eslOK> on success. <eslEOD> if stack is empty.
+ */
+int
+esl_stack_IPop(ESL_STACK *ns, int *ret_x)
+{
+  if (ns->n == 0) {*ret_x = 0; return eslEOD;}
+  ns->n--;
+  *ret_x = ns->idata[ns->n];
+  return eslOK;
+}
+
+/* Function:  esl_stack_CPop()
+ * Incept:    SRE, Sun Dec 26 09:21:27 2004 [Zaragoza]
+ *
+ * Purpose:   Pops a character off the character stack <cs>, and returns
+ *            it through <ret_c>.
+ *
+ * Returns:   <eslOK> on success. <eslEOD> if stack is empty.
+ */
+int
+esl_stack_CPop(ESL_STACK *cs, char *ret_c)
+{
+  if (cs->n == 0) {*ret_c = 0; return eslEOD;}
+  cs->n--;
+  *ret_c = cs->cdata[cs->n];
+  return eslOK;
+}
+
+/* Function:  esl_stack_PPop()
+ * Incept:    SRE, Sun Dec 26 09:21:56 2004 [Zaragoza]
+ *
+ * Purpose:   Pops a pointer off the pointer stack <ps>, and returns
+ *            it through <ret_p>.
+ *
+ * Returns:   <eslOK> on success. <eslEOD> if stack is empty.
+ */
+int
+esl_stack_PPop(ESL_STACK *ps, void **ret_p)
+{
+  if (ps->n == 0) {*ret_p = 0; return eslEOD;}
+  ps->n--;
+  *ret_p = ps->pdata[ps->n];
+  return eslOK;
+}
+
+/* Function:  esl_stack_ObjectCount()
+ * Incept:    SRE, Sun Dec 26 09:22:41 2004 [Zaragoza]
+ *
+ * Purpose:   Returns the number of data objects stored in the
+ *            stack <s>. The stack may be of any datatype.
+ */
+int 
+esl_stack_ObjectCount(ESL_STACK *s)
+{
+  return s->n;
+}
+
+
+/* Function:  esl_stack_Convert2String()
+ * Incept:    SRE, Sun Dec 26 09:23:36 2004 [Zaragoza]
+ *
+ * Purpose:   Converts a character stack <cs> to a NUL-terminated
+ *            string, and returns a pointer to the string. The
+ *            characters in the string are in the same order they
+ *            were pushed onto the stack.  The stack is destroyed by
+ *            this operation, as if <esl_stack_Destroy()> had been
+ *            called on it. The caller becomes responsible for
+ *            free'ing the returned string.
+ *
+ * Returns:   Pointer to the string; caller must <free()> this.
+ *
+ * Throws:    NULL if a reallocation fails.
+ */
+char *
+esl_stack_Convert2String(ESL_STACK *cs)
+{
+  char *s;
+
+  if (esl_stack_CPush(cs, '\0') != eslOK)
+    { free(cs->cdata); free(cs); return NULL; } /* nul-terminate the data or self-destruct */
+  s = cs->cdata;		           /* data is already just a string - just return ptr to it */
+  free(cs);			           /* free the stack around it. */
+  return s;
+}
+
+/* Function:  esl_stack_DiscardTopN()
+ * Incept:    SRE, Tue Dec 28 04:33:06 2004 [St. Louis]
+ *
+ * Purpose:   Throw away the top <n> elements on stack <s>.
+ *            Equivalent to <n> calls to a <Pop()> function.
+ *            If <n> equals or exceeds the number of elements 
+ *            currently in the stack, the stack is emptied
+ *            as if <esl_stack_Reuse()> had been called.
+ *
+ * Returns:   <eslOK> on success.
+ */
+int
+esl_stack_DiscardTopN(ESL_STACK *s, int n)
+{
+  if (n <= s->n) s->n -= n;
+  else           s->n = 0;
+  return eslOK;
+}
+
+
+/*****************************************************************
+ * Test driver and API example for the pushdown stack module.
+ * To compile:
+ *    gcc -g -Wall -I. -L. -DeslSTACK_TESTDRIVE -o testdrive esl_stack.c -leasel -lm
+ * To run:
+ *    ./testdrive
+ * Returns 0 (success) w/ no output, or returns nonzero and says why.
+ *****************************************************************/
+#ifdef eslSTACK_TESTDRIVE
+int 
+main(void)
+{
+  ESL_STACK *s = NULL;
+  int       *obj;
+  int        x;
+  char       c;
+  char      *str;
+  int        n1, n2;
+  int        i;
+  int        status;
+
+  /* Exercise of integer stacks.
+   * 
+   * Put 257 integers on the stack and pop them off;
+   * do this twice, once with a "while pop" loop, and once
+   * with a "while stack not empty" loop.
+   *
+   * With the default ALLOCSIZE of 128, putting 257 objects on the
+   * stack forces two reallocations.
+   * 
+   */
+  /* creation: */
+  if ((s = esl_stack_ICreate()) == NULL) 
+    { fprintf(stderr, "memory allocation failed\n"); return 1; }
+
+  /* pushing data: */
+  n1 = 257;
+  for (i = 0; i < n1; i++)
+    if (esl_stack_IPush(s, i) != eslOK) ESL_EXCEPTION(eslEMEM, "push failed");
+
+  /* popping data: */
+  n2 = 0;
+  while (esl_stack_IPop(s, &x) != eslEOD) n2++; 
+  if (n1 != n2)
+    { fprintf(stderr, "Put %d integers on; got %d off\n", n1, n2); return EXIT_FAILURE;}
+
+  /* same again, but use ObjectCount instead of EOD for popping
+   */
+  n1 = 257;
+  for (i = 0; i < n1; i++)
+    if (esl_stack_IPush(s, i) != eslOK) ESL_EXCEPTION(eslEMEM, "push failed");
+  n2 = 0;
+  while (esl_stack_ObjectCount(s)) {
+    if (esl_stack_IPop(s, &x) != eslOK) { fprintf(stderr, "pop failed\n"); return 1; }
+    n2++; 
+  }
+  if (n1 != n2)
+    { fprintf(stderr, "Put %d objects on; got %d off\n", n1, n2); return 1; }
+
+  /* free the stack.
+   */
+  esl_stack_Destroy(s);
+
+
+
+  /* Exercises of the ptr stack functions/API; same as above,
+   * but with pointers.
+   */
+  if ((s = esl_stack_PCreate()) == NULL) 
+    { fprintf(stderr, "memory allocation failed\n"); return 1; }
+
+  n1 = 257;
+  for (i = 0; i < n1; i++)
+    {
+      ESL_ALLOC(obj, sizeof(int) * 64);
+      if (esl_stack_PPush(s, obj) != eslOK) 
+	{ fprintf(stderr, "esl_stack_PPush failed\n"); return 1; }
+    }
+
+  n2 = 0;
+  while (esl_stack_PPop(s, (void **) &obj) != eslEOD) {
+    free(obj); 
+    n2++; 
+  }
+  if (n1 != n2)
+    { fprintf(stderr, "Put %d objects on; but popped %d off\n", n1, n2); return 1; }
+
+
+  n1 = 257;
+  for (i = 0; i < n1; i++)
+    {
+      ESL_ALLOC(obj, sizeof(int) * 64);
+
+      if (esl_stack_PPush(s, obj) != eslOK) 
+	{ fprintf(stderr, "esl_stack_PPush failed\n"); return 1; }
+    }
+  n2 = 0;
+  while (esl_stack_ObjectCount(s)) {
+    if (esl_stack_PPop(s, (void **) &obj) == eslEOD) 
+      { fprintf(stderr, "pop failed\n"); return 1; }
+    free(obj); 
+    n2++; 
+  }
+  if (n1 != n2)
+    { fprintf(stderr, "Put %d objects on; got %d off\n", n1, n2); return 1; }
+  esl_stack_Destroy(s);
+
+
+
+  /* Exercise of char stack functions/API;
+   * same as above, also including esl_stack_ToString().
+   */
+  if ((s = esl_stack_CCreate()) == NULL) 
+    { fprintf(stderr, "memory allocation failed\n"); return 1; }
+
+  n1 = 257;
+  for (i = 0; i < n1; i++)
+    if (esl_stack_CPush(s, 'X') != eslOK) ESL_EXCEPTION(eslEMEM, "push failed");
+  n2 = 0;
+  while (esl_stack_CPop(s, &c) != eslEOD) {
+    if (c != 'X') {
+      fprintf(stderr, "Put X's on; got a %c off\n", c); 
+      return 1;
+    }
+    n2++; 
+  }
+  if (n1 != n2)
+    { fprintf(stderr, "Put %d characters on; got %d off\n", n1, n2); return 1; }
+
+  for (i = 0; i < n1; i++)
+    if (esl_stack_CPush(s, 'X') != eslOK) ESL_EXCEPTION(eslEMEM, "push failed");
+  n2 = 0;
+  while (esl_stack_ObjectCount(s)) {
+    if (esl_stack_CPop(s, &c) != eslOK) { fprintf(stderr, "pop failed\n"); return 1; }
+    n2++; 
+  }
+  if (n1 != n2)
+    { fprintf(stderr, "Put %d characters on; got %d off\n", n1, n2);  return 1; }
+
+  n1 = 257;
+  for (i = 0; i < n1; i++)
+    if (esl_stack_CPush(s, 'X') != eslOK) ESL_EXCEPTION(eslEMEM, "push failed");
+
+  /* Note: the Convert2String() call destroys the stack!
+   */
+  str = esl_stack_Convert2String(s);
+  if ((n2 = strlen(str)) != n1) {
+    fprintf(stderr, "thought I'd get %d chars in that string, but got %d\n", n1, n2);
+    return 1;
+  }
+  free(str);
+  return EXIT_SUCCESS;
+
+ ERROR:
+  return status;
+}
+#endif /*eslSTACK_TESTDRIVE*/
+
+/*****************************************************************  
+ * @LICENSE@
+ *****************************************************************/
---
+++ 0.8/miniapps/esl-histplot.c
@@ -0,0 +1,204 @@
+/* esl-histplot  - collate data into a histogram and output xmgrace datafile.
+ *                  
+ * SRE, Tue Feb 21 14:18:05 2006                  
+ * SVN $Id$
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <easel.h>
+#include <esl_getopts.h>
+#include <esl_histogram.h>
+#include <esl_exponential.h>
+#include <esl_gumbel.h>
+
+
+static char banner[] = "\
+esl-histplot :: collate data histogram, output xmgrace datafile";
+
+static char usage[] = "\
+Usage: esl-histplot [-options] <datafile>";
+
+static ESL_OPTIONS options[] = {
+  /* name  type         default  env   range togs  reqs  incomp  help                docgrp */
+  {"-h",  eslARG_NONE,    FALSE, NULL, NULL, NULL, NULL, NULL, "show help and usage",                     1 },
+  {"-o",  eslARG_OUTFILE, NULL,  NULL, NULL, NULL, NULL, NULL, "output file for histogram",               1},
+
+  {"-b",  eslARG_NONE,    FALSE, NULL, NULL, NULL, NULL, NULL, "input file is binary, array of doubles",  2 },
+  {"-f",  eslARG_INT,       "1", NULL, "n>0",NULL, NULL, "-b", "which field to read on text line (1..n)", 2 },
+
+  {"-w",   eslARG_REAL,   "1.0", NULL, NULL, NULL, NULL, NULL, "bin size for histogram",                  3 },
+  {"--min", eslARG_REAL, "-100.",NULL, NULL, NULL, NULL, NULL, "initial lower bound of histogram",        3 },
+  {"--max", eslARG_REAL,  "100.",NULL, NULL, NULL, NULL, NULL, "initial upper bound of histogram",        3 },
+  {"--surv", eslARG_NONE, FALSE, NULL, NULL, NULL, NULL, NULL, "output survival plot, not histogram",     3 },
+   
+  {"--gumbel",    eslARG_NONE,FALSE, NULL, NULL, NULL, NULL, NULL,   "fit data to a Gumbel distribution",                   4 }, 
+  {"--exptail",   eslARG_NONE,FALSE, NULL, NULL, NULL, NULL, NULL,   "fit tail to an exponential distribution",             4 },
+  {"--gumloc",    eslARG_NONE,FALSE, NULL, NULL, NULL, NULL, NULL,   "fit data to a Gumbel distribution w/ known lambda",   4 }, 
+  {"--exptailloc",eslARG_NONE,FALSE, NULL, NULL, NULL, NULL, NULL,   "fit tail to an exponential tail w/ known lambda",     4 }, 
+  {"--lambda",    eslARG_REAL,"0.693",NULL,NULL, NULL, NULL, NULL,   "set known lambda",            4 },    
+  {"-p",          eslARG_REAL,"0.01", NULL,NULL, NULL, NULL, NULL,   "set tail mass to fit to",     4 },    
+
+  { 0,0,0,0,0,0,0,0,0,0},
+};
+
+
+int
+main(int argc, char **argv)
+{
+  ESL_HISTOGRAM  *h;		/* full histogram w/ collated values          */
+  ESL_GETOPTS    *go;		/* application configuration                  */
+  char           *datafile;	/* input data; "-" means stdin                */
+  FILE           *ifp;		/* input stream                               */
+  double          x;		/* value of field, after conversion to double */
+
+  int             which_field;	/* which field to use as data, 1..nf (default 1)*/
+  char           *outfile;	/* output xmgrace xy data file                  */
+  FILE           *ofp;		/* output data stream                           */
+  double          hmin;		/* initial histogram lower bound                */
+  double          hmax;		/* initial histogram upper bound                */
+  double          hbinsize;	/* histogram's bin size                         */
+
+  double *xv;
+  int     n;
+  double  params[2];
+  double  lambda;
+  double  tailp;
+
+  /*****************************************************************
+   * Parse the command line
+   *****************************************************************/
+
+  go = esl_getopts_Create(options, usage);
+  esl_opt_ProcessCmdline(go, argc, argv);
+  esl_opt_VerifyConfig(go);
+  if (esl_opt_IsSet(go, "-h")) {
+    puts(usage);
+    puts("\nGeneral options are:");
+    esl_opt_DisplayHelp(stdout, go, 1, 2, 80); /* 0= group; 2 = indentation; 80=textwidth*/
+    puts("\nOptions that control how to read the input file:");
+    esl_opt_DisplayHelp(stdout, go, 2, 2, 80); /* 1= group; 2 = indentation; 80=textwidth*/
+    puts("\nOptions that control how to display the output XY file:");
+    esl_opt_DisplayHelp(stdout, go, 3, 2, 80); /* 2= group; 2 = indentation; 80=textwidth*/
+    puts("\nOptional ML fitting of the data to distributions:");
+    esl_opt_DisplayHelp(stdout, go, 4, 2, 80); /* 3= group; 2 = indentation; 80=textwidth*/
+    return eslOK;
+  }
+  esl_opt_GetIntegerOption(go, "-f",         &which_field);
+  esl_opt_GetStringOption (go, "-o",         &outfile);
+  esl_opt_GetDoubleOption (go, "-p",         &tailp);
+  esl_opt_GetDoubleOption (go, "-w",         &hbinsize);
+  esl_opt_GetDoubleOption (go, "--min",      &hmin);
+  esl_opt_GetDoubleOption (go, "--max",      &hmax);
+  esl_opt_GetDoubleOption (go, "--lambda",   &lambda);
+
+  if (esl_opt_ArgNumber(go) != 1) 
+    esl_fatal("Incorrect number of command line arguments.\n%s\n", usage); 
+
+  datafile = esl_opt_GetCmdlineArg(go, eslARG_INFILE, NULL);
+
+
+  /*****************************************************************
+   * Open the input and output datafiles, and init the histogram.
+   *****************************************************************/
+
+  if (strcmp(datafile, "-") == 0) ifp = stdin;
+  else {
+    ifp = fopen(datafile, "rb");
+    if (ifp == NULL) esl_fatal("Failed to open input data file %s\n", datafile);
+  }
+
+  if (outfile == NULL) ofp = stdout;
+  else {
+    ofp = fopen(outfile, "w");
+    if (ofp == NULL) esl_fatal("Failed to open output xmgrace data file %s\n", outfile);
+  }
+
+  h = esl_histogram_CreateFull(hmin,hmax,hbinsize);
+  if (h == NULL) esl_fatal("Failed to create histogram");
+
+
+  /*****************************************************************
+   * Collect the data
+   *****************************************************************/
+
+  if (esl_opt_IsSet(go, "-b"))
+    {
+      while (fread(&x, sizeof(double), 1, ifp) == 1)
+	esl_histogram_Add(h, x);
+    } 
+  else 
+    {
+      char           *buf;		/* ptr to line buffer, for esl_fgets()        */
+      int             nbuf;		/* allocated line lengths, for esl_fgets()    */
+      char           *s;		/* ptr to line, for esl_strtok()              */
+      char           *tok;		/* ptr to a data field on the line            */
+      int             i;		/* counter over fields                        */
+      int             toklen;	/* string length of a field                   */
+
+      buf  = NULL;
+      nbuf = 0;
+      while (esl_fgets(&buf, &nbuf, ifp) == eslOK)
+	{
+	  s = buf;
+	  for (i = 0; i < which_field; i++)
+	    {
+	      esl_strtok(&s, " \t\n", &tok, &toklen);
+	      if (tok == NULL) continue;
+	    }
+	  x = atof(tok);
+	  esl_histogram_Add(h, x);
+	}
+      free(buf);
+    }
+  
+  /*****************************************************************
+   * Optionally, fit the data
+   *****************************************************************/
+
+  if (esl_opt_IsSet(go, "--gumbel"))
+    {
+      esl_histogram_GetData(h, &xv, &n);
+      esl_gumbel_FitComplete(xv, n, &(params[0]), &(params[1]));
+      esl_histogram_SetExpect(h, &esl_gumbel_generic_cdf, &params);
+    }
+  else if (esl_opt_IsSet(go, "--gumloc"))
+    {
+      params[1] = lambda;
+      esl_histogram_GetData(h, &xv, &n);
+      esl_gumbel_FitCompleteLoc(xv, n, params[1], &(params[0]));
+      esl_histogram_SetExpect(h, &esl_gumbel_generic_cdf, &params);
+    }
+  else if (esl_opt_IsSet(go, "--exptail"))
+    {
+      esl_histogram_GetTailByMass(h, tailp, &xv, &n, NULL);
+      esl_exp_FitComplete(xv, n, &(params[0]), &(params[1]));
+      esl_histogram_SetExpectedTail(h, params[0], tailp, &esl_exp_generic_cdf, &params);
+    }
+  else if (esl_opt_IsSet(go, "--exptailloc"))
+    {
+      params[1] = lambda;
+      esl_histogram_GetTailByMass(h, tailp, &xv, &n, NULL);
+      params[0] = xv[0];	/* might be able to do better than minimum score, but this'll do */
+      esl_histogram_SetExpectedTail(h, params[0], tailp, &esl_exp_generic_cdf, &params);
+    }
+      
+  /*****************************************************************
+   * Output
+   *****************************************************************/
+
+  if   (esl_opt_IsSet(go, "--surv"))  esl_histogram_PlotSurvival(ofp, h);
+  else                                esl_histogram_Plot(ofp, h);
+
+
+  /*****************************************************************
+   * Cleanup
+   *****************************************************************/
+
+  if (outfile != NULL)            fclose(ofp);
+  if (strcmp(datafile, "-") != 0) fclose(ifp);
+  esl_histogram_Destroy(h);
+  esl_getopts_Destroy(go);
+  return 0;
+}
