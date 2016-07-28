9fab44ba0a8243f8157f56c56a5cbb1d2c0915ad
Nick Carter <npcarter@fas.harvard.edu>
2016-07-28 20:21:28 +0000

    Easel passes all tests on ARM

--- esl_avx.c
+++ esl_avx.c
@@ -1,9 +1,10 @@
+#ifdef HAVE_AVX2
 #include <stdio.h>
 #include <immintrin.h>		/* AVX2 */
 
 #include "esl_avx.h"
 #include "easel.h"
-#ifdef HAVE_AVX2
+
  void esl_print_256(__m256i var){
   uint64_t *val = (uint64_t*) &var;
     printf("%016lx %016lx %016lx %016lx\n",   val[3], val[2], val[1], val[0]);
