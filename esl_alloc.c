704ecf9954314a377fb4be31c5da6378c1db1ea2
cryptogenomicon <sean@eddylab.org>
2017-02-11 11:15:53 -0500

    Adds a benchmark driver to esl_alloc.

--- esl_alloc.c
+++ esl_alloc.c
@@ -5,8 +5,9 @@
  * Contents:
  *    1. Portable(-ish) fallback implementation of aligned allocation.
  *    2. esl_alloc_aligned(), esl_alloc_free() API calls
- *    3. Unit tests
- *    4. Test driver
+ *    3. Benchmark driver
+ *    4. Unit tests
+ *    5. Test driver
  */
 #include "esl_config.h"
 
@@ -19,7 +20,7 @@
 /*****************************************************************
  * 1. Portable(-ish) fallback implementation of aligned allocation
  *****************************************************************/
-#if defined(eslALLOC_TESTDRIVE) ||\
+#if defined(eslALLOC_TESTDRIVE) || defined (eslALLOC_BENCHMARK) || \
   (! defined(HAVE_POSIX_MEMALIGN) && ! defined(HAVE_ALIGNED_ALLOC) && ! defined(HAVE__MM_MALLOC))
 /* Only compile this section if we need it, or for the unit test driver. */
 
@@ -60,6 +61,7 @@ alloc_aligned_fallback(size_t size, size_t alignment)
 
   return (void *) p;
 }
+
 
 static void
 alloc_aligned_free_fallback(void *p)
@@ -157,9 +159,95 @@ esl_alloc_free(void *p)
 /*----------------------- end, API ------------------------------*/
 
 
+/*****************************************************************
+ * 3. Benchmark driver
+ *****************************************************************/
+#ifdef eslALLOC_BENCHMARK
+
+#include "easel.h"
+#include "esl_alloc.h"
+#include "esl_getopts.h"
+#include "esl_random.h"
+
+static ESL_OPTIONS options[] = {
+  /* name           type      default  env  range        toggles reqs incomp  help                               docgroup*/
+  { "-h",        eslARG_NONE,   FALSE, NULL, NULL,          NULL, NULL, NULL, "show brief help on version and usage",       0 },
+  { "-1",        eslARG_NONE,"default",NULL, NULL, "-1,-2,-3,-4", NULL, NULL, "1: malloc (unaligned), realloc (unaligned)", 0 },
+  { "-2",        eslARG_NONE,   FALSE, NULL, NULL, "-1,-2,-3,-4", NULL, NULL, "2: malloc (unaligned), free/fresh alloc",    0 },
+  { "-3",        eslARG_NONE,   FALSE, NULL, NULL, "-1,-2,-3,-4", NULL, NULL, "3: our aligned malloc, free/fresh alloc",    0 },
+  { "-4",        eslARG_NONE,   FALSE, NULL, NULL, "-1,-2,-3,-4", NULL, NULL, "4: posix_memalign,     free/fresh alloc",    0 },
+  { "-s",        eslARG_INT,      "0", NULL, NULL,          NULL, NULL, NULL, "set random number seed to <n>",              0 },
+  { "-N",        eslARG_INT,  "10000", NULL, NULL,          NULL, NULL, NULL, "number of iterations",                       0 },
+  { "-L",        eslARG_INT,    "100", NULL, NULL,          NULL, NULL, NULL, "number of rows",                             0 },
+  { "-M",        eslARG_INT, "500000", NULL, NULL,          NULL, NULL, NULL, "maximum alloc per row (bytes)",              0 },
+  { "-V",        eslARG_INT,     "64", NULL, NULL,          NULL, NULL, NULL, "memory alignment (bytes)",                   0 },
+  {  0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+static char usage[]  = "[-options]";
+static char banner[] = "benchmark driver for esl_alloc";
+
+int
+main(int argc, char **argv)
+{
+  ESL_GETOPTS    *go       = esl_getopts_CreateDefaultApp(options, 0, argc, argv, banner, usage);
+  ESL_RANDOMNESS *rng      = esl_randomness_CreateFast(esl_opt_GetInteger(go, "-s"));
+  int    N                 = esl_opt_GetInteger(go, "-N");
+  int    L                 = esl_opt_GetInteger(go, "-L");
+  int    M                 = esl_opt_GetInteger(go, "-M");
+  int    V                 = esl_opt_GetInteger(go, "-V");
+  int    do_realloc        = esl_opt_GetBoolean(go, "-1");
+  int    do_fresh_malloc   = esl_opt_GetBoolean(go, "-2");
+  int    do_esl_alloc      = esl_opt_GetBoolean(go, "-3");
+  int    do_posix_memalign = esl_opt_GetBoolean(go, "-4");
+  char **p;
+  int size;
+  int    i,j;
+
+#ifndef HAVE_POSIX_MEMALIGN
+  if (do_posix_memalign) esl_fatal("can't benchmark posix_memalign(): not available on this system");
+#endif
+
+  p = malloc(sizeof(char *) * L);
+  for (i = 0; i < L; i++)
+    {
+      size = 1 + esl_rnd_Roll(rng,M);
+      if      (do_realloc)      p[i] = malloc(size);
+      else if (do_fresh_malloc) p[i] = malloc(size);
+      else if (do_esl_alloc)    p[i] = alloc_aligned_fallback(size, V);
+#ifdef HAVE_POSIX_MEMALIGN      
+      else if (do_posix_memalign) posix_memalign((void **) &(p[i]), V, size);
+#endif
+    }
+  
+  for (j = 0; j < N; j++)
+    {
+      for (i = 0; i < L; i++)
+        {
+          size = 1 + esl_rnd_Roll(rng,M);
+          if      (do_realloc)        {                                    p[i] = realloc(p[i], size);             }
+          else if (do_fresh_malloc)   { free(p[i]);                        p[i] = malloc(size);                    }
+          else if (do_esl_alloc)      { alloc_aligned_free_fallback(p[i]); p[i] = alloc_aligned_fallback(size, V); }
+#ifdef HAVE_POSIX_MEMALIGN      
+          else if (do_posix_memalign) { free(p[i]); posix_memalign((void **) &(p[i]), V, size); }
+#endif
+        }
+    }
+  
+  for (i = 0; i < L; i++)
+    if (do_esl_alloc) alloc_aligned_free_fallback(p[i]);
+    else              free(p[i]);
+  free(p);
+  esl_randomness_Destroy(rng);
+  esl_getopts_Destroy(go);
+  return 0;
+}
+#endif // eslALLOC_BENCHMARK
+/*----------------- end, benchmark ------------------------------*/
+
+
 
 /*****************************************************************
- * 3. Unit tests
+ * 4. Unit tests
  *****************************************************************/
 #ifdef eslALLOC_TESTDRIVE
 #include  <limits.h>
@@ -259,7 +347,7 @@ determine_system_alignment(void)
 
 
 /*****************************************************************
- * 4. Test driver
+ * 5. Test driver
  *****************************************************************/
 #ifdef eslALLOC_TESTDRIVE
 
