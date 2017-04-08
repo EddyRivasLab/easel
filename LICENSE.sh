ffa3001320c3733f99ee90a7528d82c48ee3517b
cryptogenomicon <sean@eddylab.org>
2017-04-08 10:21:19 -0400

    Removes stray files that came in erroneously from Nick, probably in 38d50a4.

--- LICENSE.sh
+++
@@ -1,5 +0,0 @@
-export EASEL_VERSION=h4.0dev 
-export EASEL_DATE="Apr 2015"
-export EASEL_COPYRIGHT="Copyright (C) 2015 Howard Hughes Medical Institute."
-export EASEL_LICENSE="Freely distributed under the open source BSD license."
-
