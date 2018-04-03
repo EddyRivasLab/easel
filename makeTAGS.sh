bece21e476db71b4b5c5b954a25108a96c5bd257
cryptogenomicon <sean@eddylab.org>
2018-04-03 14:12:38 -0400

    Overhauls how 'make install' works.
    Now Easel only installs miniapps and their man pages.  It no longer
    installs .h headers or libeasel.a. We've really never used it that
    way.
    
    Variable substitutions in man pages (EASEL_VERSION, etc) are now done
    by ./configure, not by outmoded sedition script. Updated man page
    headers and trailers, made consistent with HMMER style.
    
    'make install' is now always verbose; quiet beautifications removed.
    
    Removed 'make dist'.
    
    Removed ifneq's from Makefiles. Aiming to have a checkout of github
    master branch act identically to a tarball, which means we won't
    remove any directories or modify any files when we make a release
    tarball. This makes the ifneq checks for documentation/, etc obsolete.
    
    Removed LICENSE.sh and LICENSE.tag.

--- makeTAGS.sh
+++ makeTAGS.sh
@@ -3,13 +3,11 @@ etags    configure.ac
 etags -a INSTALL
 etags -a LICENSE
 
-
-# Recursively add all .c, .h, .pl, *.tex, *.man
-find . -name "*.c"   -print -or -name "*.h"  -print | xargs etags -a
-find . -name "*.pl"  -print -or -name "*.pm" -print | xargs etags -a
-find . -name "*.sh"  -print                         | xargs etags -a
-find . -name "*.tex" -print                         | xargs etags -a
-find . -name "*.man" -print                         | xargs etags -a
-find . -name "*.in"  -print                         | xargs etags -a
-find . -name "*.sqc" -print                         | xargs etags -a
-find . -name "*README"    -print                    | xargs etags -a
+# Recursively add all .c, .h, .pl, *.tex, *.in
+find . -name "*.c"       -print -or -name "*.h"  -print | xargs etags -a
+find . -name "*.pl"      -print -or -name "*.pm" -print | xargs etags -a
+find . -name "*.sh"      -print                         | xargs etags -a
+find . -name "*.tex"     -print                         | xargs etags -a
+find . -name "*.in"      -print                         | xargs etags -a
+find . -name "*.sqc"     -print                         | xargs etags -a
+find . -name "*README*"  -print                         | xargs etags -a
