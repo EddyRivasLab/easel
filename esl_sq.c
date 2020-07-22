EddyRivasLab/easel

commit 91016dfd82bd906b8e4ef677ebcb3fef90824672

Workaround for a problem in retrieving length 1 reverse complement seqs.
esl_sq.h now has the following explanation:

Easel dsq coordinates are "1-offset, closed-interval, no-flag".
1-offset means 1..n, as opposed to 0..n-1. Closed-interval means that
start/end coords i..j refers to subseq x_i..x_j, not x_i..x_j-1 (as in
half-open coords of Python lists, for example).  No-flag means that we
don't keep a flag for forward vs. reverse strand on DNA/RNA sequences;
instead, start>end means reverse strand. A flaw in such a coord system
is that we cannot unambiguously represent fwd/rev strand for a
subsequence of length 1, where start=end. So in the case of start=end,
Easel always assumes fwd strand; if rev strand is needed, there will
be some sort of patchy workaround. For example, in
esl_sq_ReverseComplement(), you can revcomp an ESL_SQ of length 1, but
its source-tracking coordinate info is deleted.

esl_sq.c
@@ -1510,11 +1510,12 @@ esl_sq_XAddResidue(ESL_SQ *sq, ESL_DSQ x)
  *            data). Degenerate nucleic acid IUPAC characters are
  *            complemented appropriately.
  *
- *            The <start/end> coords in <sq> are swapped. (Note that
- *            in the unusual case of sequences of length 1,
- *            <start=end> and we can't unambiguously tell if a seq is
- *            in the reverse complement direction or not; this is a
- *            minor flaw in Easel's current coordinate handling.)
+ *            The <start/end> coords in <sq> are swapped in the
+ *            source-tracking coordinate info. If <sq> is length 1,
+ *            because we're unable to unambiguously differentiate
+ *            forward vs. reverse strand in Easel's
+ *            1-offset/closed-interval/no-flag coord system,
+ *            source-tracking coordinate info is *deleted*.
  *
  * Returns:   <eslOK> on success.
  *            
@@ -1591,6 +1592,7 @@ esl_sq_ReverseComplement(ESL_SQ *sq)
     }
 
   ESL_SWAP(sq->start, sq->end, int64_t);
+
   /* revcomp invalidates any secondary structure annotation */
   if (sq->ss != NULL) { free(sq->ss); sq->ss = NULL; }
   /* revcomp invalidates any extra residue markup */
@@ -1600,7 +1602,15 @@ esl_sq_ReverseComplement(ESL_SQ *sq)
     free(sq->xr_tag); sq->xr_tag = NULL;
     free(sq->xr);     sq->xr     = NULL;
   }   
-  
+  /* revcomp of length 1 seq invalidates source-tracking coord info */
+  if (sq->n == 1) {
+    sq->start = -1;
+    sq->end   = -1;
+    sq->C     = -1;
+    sq->W     = -1;
+    sq->L     = -1;
+    sq->source[0] = '\0';
+  }
   return status;
 
  ERROR:

esl_sq.h
@@ -77,7 +77,20 @@
  * acc, desc info is now deprecated. Cannot distinguish empty string
  * from lack of annotation. Should use NULL ptr instead. Fix this in
  * future.  (21 Nov 09 xref J5/114)
- *    
+ *
+ * Easel dsq coordinates are "1-offset, closed-interval, no-flag".
+ * 1-offset means 1..n, as opposed to 0..n-1. Closed-interval means
+ * that start/end coords i..j refers to subseq x_i..x_j, not
+ * x_i..x_j-1 (as in half-open coords of Python lists, for example).
+ * No-flag means that we don't keep a flag for forward vs. reverse
+ * strand on DNA/RNA sequences; instead, start>end means reverse
+ * strand. A flaw in such a coord system is that we cannot
+ * unambiguously represent fwd/rev strand for a subsequence of length
+ * 1, where start=end. So in the case of start=end, Easel always
+ * assumes fwd strand; if rev strand is needed, there will be some
+ * sort of patchy workaround. For example, in
+ * esl_sq_ReverseComplement(), you can revcomp an ESL_SQ of length 1,
+ * but its source-tracking coordinate info is deleted.
  */
 typedef struct {
   /*::cexcerpt::sq_sq::begin::*/
@@ -91,7 +104,7 @@ typedef struct {
   int64_t  n;              /* length of seq (or dsq) and ss                    */
   /*::cexcerpt::sq_sq::end::*/
 
-  /* Coordinate info for:                                       seq       subseq     window     info */
+  /* Source-tracking coordinate info for:                       seq       subseq     window     info */
   /*                                                           ----       ------     ------    ----- */
   int64_t  start;  /* coord of seq[0],dsq[1] on source  [1..L]    1      1<=i<=L    1<=i<=L      0   */
   int64_t  end;    /* coord of seq[n-1],dsq[n] on source[1..L]    L      1<=j<=L    1<=j<=L      0   */
@@ -109,7 +122,7 @@ typedef struct {
   int64_t  salloc;         /* alloc for seq or dsq, and ss if present          */
   int      srcalloc;	   /* allocated length for source name                 */
 
-  /* Disk offset bookkeeping:                                                  */
+  /* Disk-tracking offset bookkeeping:                                         */
   int64_t  idx;           /* ctr for which # seq this is; -1 if not counting   */
   off_t    roff;          /* record offset (start of record); -1 if none       */
   off_t    hoff;          /* offset to last byte of header; -1 if unknown      */

esl_bitfield.c
@@ -127,7 +127,7 @@ utest_randpattern(ESL_RANDOMNESS *rng)
 #endif // eslBITFIELD_TESTDRIVE
 
 /*****************************************************************
- * 2. Unit tests
+ * 3. Test driver
  *****************************************************************/
 #ifdef eslBITFIELD_TESTDRIVE
 

esl_subcmd.c
@@ -14,6 +14,38 @@
 #include "esl_getopts.h"
 #include "esl_subcmd.h"
 
+
+/* Function:  esl_subcmd_CreateDefaultApp()
+ * Synopsis:  Process cmdline options for a subcommand
+ *
+ * Purpose:   For a subcommand <sub> of main program <topcmd>, with
+ *            subcommand options <suboptions>, process a command line
+ *            <argc>/<argv>; return a new <ESL_GETOPTS> object on
+ *            success. 
+ *
+ *            If there's a problem with the user's command line, print
+ *            informative message and `exit(1)`. If the subcommand
+ *            options include `-h`, print help and `exit(0)`.
+ *
+ *            <sub> is one <ESL_SUBCMD> structure from the main
+ *            application's table of subcommands. <topcmd> is a
+ *            string, usually the same as <argv[0]>. <suboptions>
+ *            is a table of <ESL_OPTIONS> for the subcommand.
+ *
+ *            If <topcmd> is a path, e.g. "/foo/bar/easel", the
+ *            leading path is removed, leaving just "easel" as the
+ *            name of the main program.
+ *
+ * Args:      topcmd     - name of the main command, e.g. "easel" (= argv[0], probably)
+ *            sub        - ESL_SUBCMD table entry for this subcommand
+ *            suboptions - ESL_OPTIONS table for this subcommand
+ *            argc       - number of args on the commandline
+ *            argv       - array of args on the commandline
+ *
+ * Returns:   ptr to new <ESL_GETOPTS> object
+ *
+ * Throws:    NULL on allocation failures.
+ */
 ESL_GETOPTS *
 esl_subcmd_CreateDefaultApp(const char *topcmd, const ESL_SUBCMD *sub, const ESL_OPTIONS *suboptions, int argc, char **argv)
 {

commit 86e370e8c001569df0bfd19565ae0002a088bd2b

esl_sq_ReverseComplement() of length 1 seq retains source info again
Problem has to do with how ESL_SQ source coordinates work for a
reverse complemented sequence of length 1. Easel 1-offset,
closed-interval, no-flag coords have no way to distinguish when a
length 1 seq is fwd or reverse strand (42..42, for example). Commit
91016df added a note in esl_sq.h about this, and changed the behavior
of esl_sq_ReverseComplement(), so it deleted source tracking
information for a seq of length 1. At the time, I remember thinking,
this'll probably break something.

It broke something. That recent commit 91016df caused occasional
reproducible unit test failure, `esl_sqio_utest -s 197`. The problem
is that esl_sqio_ReadWindow() tracks source coords, and the unit test
verifies them; if you use esl_sqio_ReadWindow() on a sequence of
length 1, the reverse strand read has no source coords, and the unit
test fails.

Seems likely that that surprising behavior from esl_sqio_ReadWindow()
-- very rarely, on the unusual case of a length 1 sequence (or more
weirdly, the last nucleotide of a sequence of length L%W=1 when
context C=0), the ESL_SQ would be missing source coord tracking info
-- would mess up a esl_sqio_ReadWindow() user, and it's mean to have
them write special case checks for it - they already have other ways
of knowing they're reading the rev strand. Assuming that other callers
will also have alternative means of knowing that a ESL_SQ that goes
through esl_sq_ReverseComplement() is fwd vs reverse, I reverted the
previous commit's behavior. Source coord tracking is preserved, and
the unit test passes again.

esl_sq.c
@@ -1512,10 +1512,12 @@ esl_sq_XAddResidue(ESL_SQ *sq, ESL_DSQ x)
  *
  *            The <start/end> coords in <sq> are swapped in the
  *            source-tracking coordinate info. If <sq> is length 1,
- *            because we're unable to unambiguously differentiate
- *            forward vs. reverse strand in Easel's
- *            1-offset/closed-interval/no-flag coord system,
- *            source-tracking coordinate info is *deleted*.
+ *            we're unable to unambiguously differentiate forward
+ *            vs. reverse strand in Easel's
+ *            1-offset/closed-interval/no-flag coord system (see
+ *            comments in esl_sq.h), so caller needs to hack some way
+ *            of remembering for itself that the sequence is reverse
+ *            complemented.
  *
  * Returns:   <eslOK> on success.
  *            
@@ -1602,15 +1604,6 @@ esl_sq_ReverseComplement(ESL_SQ *sq)
     free(sq->xr_tag); sq->xr_tag = NULL;
     free(sq->xr);     sq->xr     = NULL;
   }   
-  /* revcomp of length 1 seq invalidates source-tracking coord info */
-  if (sq->n == 1) {
-    sq->start = -1;
-    sq->end   = -1;
-    sq->C     = -1;
-    sq->W     = -1;
-    sq->L     = -1;
-    sq->source[0] = '\0';
-  }
   return status;
 
  ERROR:

esl_sq.h
@@ -88,9 +88,7 @@
  * unambiguously represent fwd/rev strand for a subsequence of length
  * 1, where start=end. So in the case of start=end, Easel always
  * assumes fwd strand; if rev strand is needed, there will be some
- * sort of patchy workaround. For example, in
- * esl_sq_ReverseComplement(), you can revcomp an ESL_SQ of length 1,
- * but its source-tracking coordinate info is deleted.
+ * sort of patchy workaround. 
  */
 typedef struct {
   /*::cexcerpt::sq_sq::begin::*/